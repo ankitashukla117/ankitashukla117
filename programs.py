50babcf8531a2975d62b5f850b71abe4a7ffa143
ankita shukla <ankitaduggu25@gmail.com>
2024-09-03 22:10:24 +0530

    Add files via upload

---
+++ programs.py
@@ -0,0 +1,83 @@
+'''s=int(input("enter the value"))
+area =s * s
+print("area of square -:);
+print(area)'''
+
+'''l=int(input("enter the length"))
+b=int(input("enter the breadth"))
+area=(l*b)
+print("area of rectangle -:);
+print(area)'''
+
+'''l=int(input("enter the value"))
+b=int(input("enter the value"))
+h=int(input("enter the value"))
+area = 2 *(l*b+b*h+h*l) 
+print("area of cuboid -:");
+print(area)'''
+
+
+'''a=int(input("enter the value"))
+b=int(input("enter the value"))
+h=int(input("enter the value"))
+area=1/2*(a+b)+h
+print("area of cuboid is -:");
+print(area)'''
+            
+a=int(input("enter the value"))
+b=int(input("enter the value"))
+add=a+b
+print("result -:")
+print(add)
+subtract=a-b
+print("result -:")
+print(subtract)
+multiplication=a*b
+print("result -:")
+print(multiplication)
+division=a/b
+print("result -:")
+print(division)
+modulous=a%b 
+print("result -:")
+print(modulous)
+exponent=a**b
+print("result -:")
+print(exponent)
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
